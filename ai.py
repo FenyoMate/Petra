ad8906929a2345e8d04b11186d173716cd4dc362
FenyoMate <fenyo.mate.steam9913@gmail.com>
2023-10-26 18:41:52 +0200

    - Increasing Max_tokens to 30 - DB Wiped for tests

--- ai.py
+++ ai.py
@@ -12,7 +12,7 @@ def process(msg):
              "content": "You are a hungarian woman asssistant. Your job to answer the prompt questions."},
             {"role": "user", "content": msg}
         ],
-        max_tokens=20,
+        max_tokens=30,
         temperature=0.7
     )
     print(response.choices[0].message)
