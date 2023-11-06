0ceba7c7bc62b3728d0cac779bb964dbc0cc84d3
FenyoMate <79763375+FenyoMate@users.noreply.github.com>
2023-11-06 21:28:36 +0100

    Update settings.py

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'petra',
         'USER': 'admin',
         'PASSWORD': 'admin',
